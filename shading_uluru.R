07a3f3df0cbdb103764d67d52676adc0d93eb005
Miles McBain <miles.mcbain@gmail.com>
2018-11-14 12:03:11 +1000

    texture example working, added quadmesh example

--- shading_uluru.R
+++ shading_uluru.R
@@ -7,7 +7,8 @@ library(devtools)
 library(sf)
 library(raster)
 library(tidyverse)
-source("./helpers/sf_to_trimesh.R")
+library(RTriangle)
+library(r2vr.gis) ## devtools::install("milesmcbain/r2vr.gis")
 
 ### Make a tight bounding box.
 ### coords come from a google map: https://drive.google.com/open?id=1Ak26Hyx1R-f2QjPCTK0rQLye5xcHyE8n&usp=sharing
@@ -19,33 +20,22 @@ uluru_bbox <-
             ymax = -25.33568),
           crs = st_crs("+proj=longlat +ellps=WGS84"))
 
-### Convert to a MULTIPOLYGON
-uluru_bbox_mpoly <-
-  uluru_bbox %>%
-    st_as_sfc() %>%
-    st_multipolygon() %>%
-    st_geometry()
-
-st_crs(uluru_bbox_mpoly) <- st_crs(uluru_bbox)
-
 ### Read in raster
 nt_raster <- raster("./data/ELVIS_CLIP.tif")
 
 ### Homogenise bbox and raster CRS
-uluru_bbox_mpoly <-
-  st_transform(uluru_bbox_mpoly, crs = crs(nt_raster)@projargs)
+uluru_bbox_nt_crs<-
+  bbox_transform(uluru_bbox, crs = crs(nt_raster)@projargs)
 
 ### Crop raster and sanity check
 nt_raster_cropped <-
-  nt_raster %>%
-  crop(st_bbox(uluru_bbox_mpoly)[c("xmin", "xmax",
-                                   "ymin", "ymax")])
+  raster_crop_bbox(nt_raster, uluru_bbox_nt_crs)
 
 plot(nt_raster_cropped) ## looks good!
 
 ### Triangulate bbox
 uluru_bbox_trimesh <-
-  sf_to_trimesh(uluru_bbox_mpoly, 12000) # a few more than last example for finer mesh.
+  bbox_to_trimesh(uluru_bbox_nt_crs, 12000) # a few more than last example for finer mesh.
 
 ### Add elevation to trimesh
 ul_extent_elev <-
@@ -56,8 +46,11 @@ uluru_bbox_trimesh$P <-
 ### Calculating height correction factor for VR, discussed in previous post.
 ## We need to correct the height based on ground height.
 ## In this case we'll find ground height from the  highest corner of the bounding box.
-ground_height <-
-  max(raster::extract(nt_raster, uluru_bbox_mpoly[[1]][[1]][[1]], nrow = 1))
+
+bbox_corners <-
+  bbox_to_multipoly(uluru_bbox_nt_crs)[[1]][[1]][[1]]
+
+ground_height <- max(raster::extract(nt_raster, bbox_corners))
 
 height_correction <- -1 * (ground_height - mean(uluru_bbox_trimesh$P[, 3]))
 ## We're reversing the correction that would have been applied to the
@@ -170,51 +163,48 @@ aframe_scene2$stop()
 
 
 ### Using a texture
-library(dismo)
 library(fs)
-
-## there are better ways to get imagery, but this is a good old faithful detault
-im <- dismo::gmap(nt_raster_cropped, type = "satellite", scale = 2)
-
-## Look at using mapbox instead of dismo
 library(slippymath)
 library(curl)
 library(glue)
 
-## Try to get a tile from mapbox as a test
-lat <- uluru_bbox[c("xmin")]
-lon <- uluru_bbox[c("ymin")]
-zoom <- 11
-## Zoom 11 should fit Uluru in 1 tile
+## Fetch a tile from mapbox calculate the tiles with slippymath
+uluru_tiles <- bb_to_tg(uluru_bbox, max_tiles = 40)
 
-tile_nums <- latlon_to_tilenum(lat, lon, zoom)
-x <- tile_nums$x
-api_key <- Sys.getenv("MAPBOX_API_KEY")
+mapbox_query_string <-
+  paste0("https://api.mapbox.com/v4/mapbox.satellite/{zoom}/{x}/{y}.jpg90",
+         "?access_token=",
+         Sys.getenv("MAPBOX_API_KEY"))
 
-mapbox_url <- glue("https://api.mapbox.com/v4/mapbox.satellite/{zoom}/{tile_nums$x}/{tile_nums$y}.jpg90?access_token={api_key}")
+images <-
+  pmap(uluru_tiles$tiles,
+       function(x, y, zoom){
+         outfile <- glue("{x}_{y}.jpg")
+         curl_download(url = glue(mapbox_query_string),
+                       destfile = outfile) 
+         outfile 
+       },
+       zoom = uluru_tiles$zoom)
 
+## composite images with slippymath
+uluru_raster <- tg_composite(uluru_tiles, images)
 
-## so this is a bit head-stretching, because multiple coordinate systems in play
+## Crop the image
+uluru_raster <- raster_crop_bbox(uluru_raster, uluru_bbox)
 
-## we want the [0,1,0,1] coordinates of the image in terms of the geographic mesh
-## 1) mesh is lcc, backwards to longlat WGS84
-xyl <- rgdal::project(uluru_bbox_trimesh$P[,1:2], projection(nt_raster), inv = TRUE)
-
-## 2) image is merc, forwards to that
-xym <- rgdal::project(xyl, projection(im))
-
-## 3) the cell (in [0,1,0,1]) for our mesh coordinates
-cell <- cellFromXY(im, xym)
-
-## 4) use that cell to get native from a rescaled im
-xyim <- xyFromCell(setExtent(im, extent(0, 1, 0, 1)), cell)
-
-## 5) convert to RGB from palette (might be a raster fun for this...)
-imrgb <- setValues(brick(im, im, im), t(col2rgb(im@legend@colortable[values(im) + 1])))
-
-## 6) write to PNG (that's the only way we can texture map)
+## write to PNG (that's the only way we can texture map)
 texfile <- "./data/uluru_satellite.png"
-rgdal::writeGDAL(as(imrgb, "SpatialGridDataFrame"), texfile, driver = "PNG")
+raster_to_png(uluru_raster, texfile)
+
+## calculate the trimesh x and y in 0 - 1 space for texture coordinates
+## using r2vr.gis::range_scale
+xym <- uluru_bbox_trimesh$P[,1:2]
+
+xyim <- apply(xym, 2, range_scale)
+## This works because the image and the mesh have the same area and the area is
+## relatively small, so the won't be much opportunity for the texture to be
+## distorted by difference in projection.
+
 
 ## generate JSON containing texture
 ## pass just the name of the texture file so rendering process will look in the
@@ -233,25 +223,74 @@ readr::write_file(uluru_tex_json, "./data/uluru_tex.json")
 ## The JSON references the satellite png file so it needs to be included in 'parts'.
 uluru_tex <- a_asset(src = "./data/uluru_tex.json",
                      id = "uluru_tex",
-                     parts = "./data/uluru_satellite.png")
+                     .parts = "./data/uluru_satellite.png")
 
 uluru <-
-  a_json_model(src_asset = uluru_tex,
+  a_json_model(src = uluru_tex,
                mesh_smooth = TRUE,
                scale = scale_factor * c(1, 1, 1),
                position = c(0, 0 + height_correction * scale_factor, -15),
                rotation = c(-90, 180, 0))
 
 aframe_scene3 <-
-  a_scene(template = "empty",
-          title = "Uluru Mesh",
-          description = "An A-Frame scene of Uluru",
-          children = list(uluru, sky, controls))
+  a_scene(.template = "empty",
+          .title = "Uluru Mesh",
+          .description = "An A-Frame scene of Uluru",
+          .children = list(uluru, sky, controls))
 
 aframe_scene3$serve()
 
 ## don't forget to:
 aframe_scene3$stop()
+
+## Bonus: quadmesh
+## devtools::install_github("mdsumner/quadmesh")
+library(quadmesh)
+
+qm <- quadmesh(nt_raster_cropped)
+
+triangulate_quads <- function(quad_index) {
+  matrix(rbind(quad_index[c(1L, 4L, 2L), ], quad_index[c(4L, 3L, 2L), ]), 3L)
+}
+
+trimesh <- triangulate_quads(qm$ib)
+## reuse same vertices with new indicies - cut quads in half along diagonals
+
+
+vertices_columns <- t(qm$vb[1:3, ])
+faces_columns <- t(trimesh)
+texture_coords <- apply(vertices_columns[ , 1:2], 2, range_scale)
+
+mesh_json <- trimesh_to_threejson(vertices = vertices_columns,
+                                  face_vertices = faces_columns,
+                                  vertex_uvs = texture_coords,
+                                  texture_file = fs::path_file(texfile))
+
+readr::write_file(mesh_json, "./data/uluru_tex.json")
+
+## VR test
+## The JSON references the satellite png file so it needs to be included in 'parts'.
+uluru_q_tex <- a_asset(src = "./data/uluru_tex.json",
+                       id = "uluru_tex",
+                       .parts = "./data/uluru_satellite.png" )
+uluru_quads <-
+  a_json_model(src = uluru_q_tex,
+               mesh_smooth = TRUE,
+               scale = scale_factor * c(1, 1, 1),
+               position = c(0, 0 + height_correction * scale_factor, -15),
+               rotation = c(-90, 180, 0))
+
+aframe_scene4 <-
+  a_scene(.template = "empty",
+          .title = "Uluru Mesh",
+          .description = "An A-Frame scene of Uluru",
+          .children = list(uluru_quads, sky, controls))
+
+aframe_scene4$serve()
+
+
+aframe_scene4$stop()
+
 
 ## Bonus: rayshader
 ## devtools::install_github("diligently/rayshader")
@@ -286,13 +325,13 @@ uluru_tex <- a_in_mem_asset(data = list(uluru_tex_json, readr::read_file_raw(tex
                             id = "uluru_tex",
                             parts = "./uluru_shade.png")
 
-aframe_scene4 <-
+aframe_scene5 <-
   a_scene(template = "empty",
           title = "Uluru Mesh",
           description = "An A-Frame scene of Uluru",
           children = list(uluru, sky, a_pc_control_camera()))
 
-aframe_scene4$serve()
+aframe_scene5$serve()
 
 ## don't forget to:
-aframe_scene4$stop()
+aframe_scene5$stop()
